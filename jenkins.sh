b14a04f8b105afa19ebd4ad473c56174db93b7a9
Jesper Wermuth <wermuth@lundogbendsen.dk>
2014-09-07 08:39:14 +0200

    Only get from git if its not already there

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-#sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-#sudo apt-get update
-#sudo apt-get -y install jenkins
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get -y install jenkins
 
 # Pull Jenkins configuration from git
 sudo su jenkins
 cd /var/lib/jenkins
 echo In directory `pwd`
-sudo su - jenkins -c "rm -rf tmp && git clone https://github.com/jwermuth/basecamp-jenkins.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
+if [ ! -f .git ]
+then 
+    sudo su - jenkins -c "rm -rf tmp && git clone https://github.com/jwermuth/basecamp-jenkins.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
+fi
