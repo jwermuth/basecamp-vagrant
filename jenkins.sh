4bd4620eaecbcad4030068139bc3ce3b8a887be9
Jesper Wermuth <wermuth@lundogbendsen.dk>
2014-09-07 08:42:38 +0200

    restart after getting stuff from git

--- jenkins.sh
+++ jenkins.sh
@@ -12,4 +12,7 @@ echo In directory `pwd`
 if [ ! -f .git ]
 then 
     sudo su - jenkins -c "rm -rf tmp && git clone https://github.com/jwermuth/basecamp-jenkins.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
+    sudo service jenkins restart
 fi
+
+
