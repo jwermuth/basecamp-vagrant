9802abcc9671469bd91af3c77b97aa0a4e145447
Jesper Wermuth <wermuth@lundogbendsen.dk>
2014-12-20 17:57:45 +0100

    Use wermuth user as jenkins push'er

--- jenkins-configure.sh
+++ jenkins-configure.sh
@@ -6,9 +6,9 @@ cd /var/lib/jenkins
 echo Jenkins-configure.sh says: In directory `pwd`
 if [ ! -f .git ]
 then 
-    sudo su - jenkins -c "rm -rf tmp && rm -rf .git && git clone https://github.com/jwermuth/basecamp-jenkins.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
-    git config user.email "jenkins@localhost.com"
-    git config user.name "jenkins"
+    sudo su - jenkins -c "rm -rf tmp && git clone https://github.com/jwermuth/basecamp-jenkins.git tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
+    git config user.email "jwermuth@gmail.com"
+    git config user.name "jwermuth"
 
 	echo Jenkins-configure.sh says: Attempting to restart Jenkins
     sudo service jenkins restart
