02166d7cb522ed545fba675e0c561a218b8a1022
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-01-04 17:16:45 +0100

    git user configured globally

--- jenkins-configure.sh
+++ jenkins-configure.sh
@@ -15,8 +15,8 @@ if [ ! -d .git ]
 then 
 	echo There is no git here, so Ill clone jenkins configuration from github
     sudo su - jenkins -c "rm -rf tmp && git clone $GIT_JENKINS_CONFIGURATION_REPO tmp && mv tmp/.git . && rm -rf tmp && git reset --hard"
-    sudo su - jenkins -c "git config user.email $EMAIL"
-    sudo su - jenkins -c "git config user.name $GIT_USER"
+    sudo su - jenkins -c "git config --global user.email $EMAIL"
+    sudo su - jenkins -c "git config --global user.name $GIT_USER"
 
 	echo Jenkins-configure.sh says: Attempting to restart Jenkins
     sudo service jenkins restart
