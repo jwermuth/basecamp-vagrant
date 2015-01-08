f577045c272fdf8f446ea0e551f543c4c9d39f98
Jesper Wermuth <wermuth@lundogbendsen.dk>
2015-01-08 21:55:26 +0100

    Developer user in sudo

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,18 @@ group {'developer':
 	ensure => present,
 }
 
-user {'developer':
+# The @ marks a virtual resource. 
+# A virtual resource is similar to a reference
+@user {'developer':
 	ensure => present,
-	gid => 'developer',
-	shell => '/bin/bash',
-	home => '/home/developer',
+	gid    => 'developer',
+	shell  => '/bin/bash',
+	home   => '/home/developer',
+	groups  => ["sudo"],
 	managehome => 'true',
+	# password is 'developer'
 	password => '$6$HYilSZKDMYH.7i$nEuPmDOJmuCYVWMWuUP7efFxAl0PHs4QQpNLWyQw42Fg4dhDMhydQbpsKUNgcAtyCOq/FU8YPYfjE/lz4p.L/0',
 }
+# Here we are using (sending to the client) the 
+# User resource defined in the @ Virtual resource style above
+realize User[developer]
